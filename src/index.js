68d72d3248d6d64d77f68ee0c663824e0d5dd78e
MelllivoraCapensis <__>
2018-09-17 02:07:54 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+module.exports=function getZerosCount(number) {
+
+	function count_degree(divisor)
+	{
+	var max_degree=0;
+	while (Math.pow(divisor,max_degree)<=number) {
+		max_degree++;
+	}
+	max_degree--;
+    count_numbers=0;
+    count_degrees=0;
+    for(var i=max_degree;i>0;i--)
+    {
+    	count_degrees+=i*(Math.floor(number/Math.pow(divisor,i))-count_numbers);
+    	count_numbers=Math.floor(number/Math.pow(divisor,i));
+    }
+    return count_degrees;
+    }
+    return Math.min(count_degree(2),count_degree(5));
 }
+
+
